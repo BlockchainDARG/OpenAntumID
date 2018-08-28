91e8662b90d5e08f191199feab896f0ddb6ebf5e
JMUATANTUM <JMUATANTUM@users.noreply.github.com>
2018-08-28 14:12:49 +0200

    Resolving correct client IP-address (ajax.php)

--- ajax.php
+++ ajax.php
@@ -17,11 +17,35 @@ limitations under the License.
 require_once dirname(__FILE__) . "/DAO.php";
 $dao = new DAO();
 // check if this nonce is logged or not
-$address = $dao->address($_POST['nonce'], @$_SERVER['REMOTE_ADDR']);
+// $address = $dao->address($_POST['nonce'], @$_SERVER['REMOTE_ADDR']);
+
+$address = $dao->address($_POST['nonce'], get_client_ip()); // Resolve correct client IP.
+
 if($address!==false) {
     // Create session so the user could log in
     session_start();
     $_SESSION['user_id'] = $address;
 }
+
+// (AntumID Development) Optimized CODE for correct IP - 21-08-2018
+function get_client_ip() {
+    $ipaddress = '';
+    if (getenv('HTTP_CLIENT_IP'))
+        $ipaddress = getenv('HTTP_CLIENT_IP');
+    else if(getenv('HTTP_X_FORWARDED_FOR'))
+        $ipaddress = getenv('HTTP_X_FORWARDED_FOR');
+    else if(getenv('HTTP_X_FORWARDED'))
+        $ipaddress = getenv('HTTP_X_FORWARDED');
+    else if(getenv('HTTP_FORWARDED_FOR'))
+        $ipaddress = getenv('HTTP_FORWARDED_FOR');
+    else if(getenv('HTTP_FORWARDED'))
+       $ipaddress = getenv('HTTP_FORWARDED');
+    else if(getenv('REMOTE_ADDR'))
+        $ipaddress = getenv('REMOTE_ADDR');
+    else
+        $ipaddress = 'UNKNOWN';
+    return $ipaddress;
+	}
+
 //return address/false to tell the VIEW it could log in now or not
 echo json_encode($address);
